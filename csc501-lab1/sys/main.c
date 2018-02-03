cee8959b24ea9d5aa21a0d2722f5c00fe6170a7d
skywhat <i@skywhat.com>
2018-02-03 16:45:19 -0500

    modify main

--- csc501-lab1/sys/main.c
+++ csc501-lab1/sys/main.c
@@ -24,10 +24,20 @@ int main()
 	resume(prB=create(prch,2000,20,"proc B",1,'B'));
 	resume(prC=create(prch,2000,20,"proc C",1,'C'));
 
-	while(1){
-		kprintf("%c",'D');
-		for(i=0;i<10000;i++);
+	for(i=0;i<NPROC;++i){
+		if(i!=0&&isbadpid(i)){
+			return SYSERR;
+		}
+		if(proctab[i].pstate==PRFREE){
+			continue;
+		}
+		kprintf("\nProcess [%s]\n",proctab[i].pname);
+		kprintf("\tpriority: %d\n",proctab[i].pprio);
+
+		suspend(i);
+
 	}
+
 
 	return 0;
 }
