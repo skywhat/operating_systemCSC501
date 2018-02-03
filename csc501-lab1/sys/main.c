b2b9a52825deb67f6414fb2aeb3b83a5b213a1ae
skywhat <i@skywhat.com>
2018-02-03 16:25:18 -0500

    initial commit for lab1

--- csc501-lab1/sys/main.c
+++ csc501-lab1/sys/main.c
@@ -5,12 +5,37 @@
 #include <proc.h>
 #include <stdio.h>
 
+#include<q.h>
+
 /*------------------------------------------------------------------------
  *  main  --  user main program
  *------------------------------------------------------------------------
  */
+
+int prch(),prA,prB,prC;
+
 int main()
 {
 	kprintf("\n\nHello World, Xinu lives\n\n");
+
+	int i;
+	kprintf("\n\nTEST1:\n");
+	resume(prA=create(prch,2000,20,"proc A",1,'A'));
+	resume(prB=create(prch,2000,20,"proc B",1,'B'));
+	resume(prC=create(prch,2000,20,"proc C",1,'C'));
+
+	while(1){
+		kprintf("%c",'D');
+		for(i=0;i<10000;i++);
+	}
+
 	return 0;
+}
+prch(c) char c;
+{
+	int i;
+	while(1){
+		kprintf("%c",c);
+		for(i=0;i<10000;i++);
+	}
 }
