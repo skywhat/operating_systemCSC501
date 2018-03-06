1644ee04b57c8e900338c6b1f8133aba6db48938
skywhat <i@skywhat.com>
2018-03-06 18:18:58 -0500

    update

--- csc501-lab2-qemu/sys/task1.c
+++ csc501-lab2-qemu/sys/task1.c
@@ -31,22 +31,36 @@ void writer2(char msg,int sem){
 
 }
 
+void writer(char msg){
+  kprintf(" %c start to write\n",msg);
+  sleep(3);
+  kprintf(" %c write done.\n",msg);
+}
+
 int main(){
   kprintf("lock with priority inheritance\n");
   int lck=lcreate();
-  int w1a=create(writer1,2000,20,"writer1",3,"A",lck,20);
-  int w1b=create(writer1,2000,30,"writer1",3,"B",lck,20);
+  int w1a=create(writer1,2000,20,"writer1",3,'A',lck,20);
+  int w1b=create(writer1,2000,30,"writer1",3,'B',lck,20);
+  int wc=create(writer,2000,25,"writer",1,'C');
   resume(w1a);
+  sleep(1);
   resume(w1b);
+  sleep(1);
+  resume(wc);
 
   sleep(10);
 
   kprintf("semaphore without priority inheritance\n");
   int sem=screate(1);
-  int w2a=create(writer2,2000,20,"writer2",2,"A",sem);
-  int w2b=create(writer2,2000,30,"writer2",2,"B",sem);
+  int w2a=create(writer2,2000,20,"writer2",2,'A',sem);
+  int w2b=create(writer2,2000,30,"writer2",2,'B',sem);
+  int wc=create(writer,2000,25,"writer",1,'C');
   resume(w2a);
+  sleep(1);
   resume(w2b);
+  sleep(1);
+  resume(wc);
 
 
   return 0;
