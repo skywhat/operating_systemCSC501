130fa515a05691007ee5356f7a2d83b67e33d5e8
skywhat <i@skywhat.com>
2018-03-07 19:05:23 -0500

    final version

--- csc501-lab2-qemu/autoTMP.sh
+++ csc501-lab2-qemu/autoTMP.sh
@@ -2,7 +2,7 @@
 
 h_files=(lock.h proc.h q.h)
 sys_files=(linit.c lcreate.c ldelete.c lock.c releaseall.c initialize.c resched.c getprio.c create.c kill.c getprio.c chprio.c task1.c release.c admitreaders.c)
-
+compile_files=(Makefile)
 
 i="0"
 while [ $i -lt ${#h_files[@]} ]
@@ -18,5 +18,13 @@ while [ $i -lt ${#sys_files[@]} ]
 do
 	echo "cp ./sys/${sys_files[i]} ./TMP/"
 	cp ./sys/${sys_files[i]} ./TMP/
+	i=$[$i+1]
+done
+
+i="0"
+while [ $i -lt ${#compile_files[@]} ]
+do
+	echo "cp ./compile/${compile_files[i]} ./TMP/"
+	cp ./compile/${compile_files[i]} ./TMP/
 	i=$[$i+1]
 done
